ca2f5f0d5edf89faac4b73da71272135c24d0126
simpart <simpart>
2020-06-01 16:19:46 +0900

    mod setup

--- setup/setup.sh
+++ setup/setup.sh
@@ -6,15 +6,16 @@ apt-get install -y apache2 libusb-1.0-0-dev cmake libssl-dev
 # init apache2
 systemctl enable apache2
 
+mv $SCP_DIR/../../seekgate /var/www/html/
+
 # install seekthermal library
 git clone https://github.com/simpart/seekutil.git /usr/local/src/seekutil
 cd /usr/local/src/seekutil/src/cxx/
 make && make install
 
 # install websocket library
-cd /usr/local/src
 git clone https://github.com/warmcat/libwebsockets.git /usr/local/src/libwebsockets
-mkdir libwebsockets/build
+mkdir /usr/local/src/libwebsockets/build
 cd /usr/local/src/libwebsockets/build
 cmake ..
 make && make install
@@ -22,7 +23,7 @@ cp $SCP_DIR/libwebsockets.conf  /etc/ld.so.conf.d/
 ldconfig
 
 # build seekgate
-cd $SCP_DIR/src/cxx
+cd $SCP_DIR/../src/cxx
 make
 cp ./seekgate /usr/local/sbin/
 
@@ -31,6 +32,7 @@ a2enmod cgid
 cp $SCP_DIR/../src/py/threshold.py /usr/lib/cgi-bin/
 cp $SCP_DIR/../src/py/conf.json /usr/lib/cgi-bin/
 chown www-data:www-data /usr/lib/cgi-bin/conf.json
+systemctl restart apache2
 
 # set daemon
 cp $SCP_DIR/seekgate.service /etc/systemd/system/
